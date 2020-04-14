9f3a8ca2d096423afefec6ce3dad4240c2bca56a
hikaru7719 <j148016s@st.u-gakugei.ac.jp>
2020-04-14 23:59:21 +0900

    rename gita tag version

--- src/git.rs
+++ src/git.rs
@@ -4,7 +4,7 @@ pub fn new_git() -> Command {
     Command::new("git")
 }
 
-pub fn git_tag(version: &str) -> std::io::Result<Output> {
+pub fn git_tag_version(version: &str) -> std::io::Result<Output> {
     let mut cmd = new_git();
     cmd.arg("tag").arg(version).output()
 }
@@ -26,7 +26,7 @@ mod tests {
     #[test]
     fn test_git_tag() {
         let version_str = "test-git-tag";
-        git_tag(version_str).unwrap();
+        git_tag_version(version_str).unwrap();
         let result = git_tag_delete(version_str).unwrap();
         let output = String::from_utf8_lossy(&result.stdout).to_string();
         assert!(output.contains("Deleted tag 'test-git-tag'"), true);
