5499f2c285f1b778cbb272c506ed9fa98ab7ee6e
hikaru7719 <j148016s@st.u-gakugei.ac.jp>
2020-04-15 10:30:18 +0900

    git tag list

--- src/git.rs
+++ src/git.rs
@@ -4,6 +4,11 @@ pub fn new_git() -> Command {
     Command::new("git")
 }
 
+pub fn git_tag() -> std::io::Result<Output> {
+    let mut cmd = new_git();
+    cmd.arg("tag").output()
+}
+
 pub fn git_tag_version(version: &str) -> std::io::Result<Output> {
     let mut cmd = new_git();
     cmd.arg("tag").arg(version).output()
@@ -14,7 +19,7 @@ fn git_tag_delete(version: &str) -> std::io::Result<Output> {
     cmd.arg("tag").arg("-d").arg(version).output()
 }
 
-fn git_fetch() -> std::io::Result<Output> {
+pub fn git_fetch() -> std::io::Result<Output> {
     let mut cmd = new_git();
     cmd.arg("fetch").output()
 }
@@ -25,6 +30,14 @@ mod tests {
 
     #[test]
     fn test_git_tag() {
+        let result = git_tag().unwrap();
+        let string = String::from_utf8(result.stdout).unwrap();
+        println!("{}", string);
+        assert_eq!(result.status.code().unwrap(), 0);
+    }
+
+    #[test]
+    fn test_git_tag_version() {
         let version_str = "test-git-tag";
         git_tag_version(version_str).unwrap();
         let result = git_tag_delete(version_str).unwrap();
