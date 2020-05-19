95c39ca7ad59271e97cd636abbb04abcd45fdfc0
miyahika <j148016s@st.u-gakugei.ac.jp>
2020-05-20 00:39:01 +0900

    add clap

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+extern crate clap;
+use clap::{App, Arg};
 fn main() {
-    println!("Hello, world!");
+    let app = App::new("git-tag-push")
+        .version("0.1.0")
+        .about("git tag push is cli to upgrade version keeping form of semantic versioning and push version to remote.")
+        .arg(
+            Arg::with_name("prefix")
+                .about("version prefix")
+                .short('p')
+                .long("prefix"),
+        )
+        .arg(
+            Arg::with_name("sufix")
+                .about("version sufix")
+                .short('s')
+                .long("sufix"),
+        )
+        .subcommand(App::new("major"))
+        .subcommand(App::new("minor"))
+        .subcommand(App::new("patch"));
 }
