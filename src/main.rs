15625c314bb93c3641298c96b91b266d097619d5
miyahika <j148016s@st.u-gakugei.ac.jp>
2020-05-20 01:06:54 +0900

    add command line option

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 extern crate clap;
+extern crate git_tag_push;
 use clap::{App, Arg};
+use git_tag_push::config::Config;
 fn main() {
+    let mut config = Config::new();
     let app = App::new("git-tag-push")
         .version("0.1.0")
         .about("git tag push is cli to upgrade version keeping form of semantic versioning and push version to remote.")
@@ -15,6 +18,18 @@ fn main() {
                 .about("version sufix")
                 .short('s')
                 .long("sufix"),
+        )
+        .arg(
+            Arg::with_name("auto-push")
+            .about("enablet to push remote")
+            .short('P')
+            .long("auto-push")
+        )
+        .arg(
+            Arg::with_name("fetch")
+            .about("whether fetch or not before tag commit")
+            .short('f')
+            .long("fetch")
         )
         .subcommand(App::new("major"))
         .subcommand(App::new("minor"))
