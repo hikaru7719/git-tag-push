c6188981a89910236598129b08e111b66f4232f4
miyahika <j148016s@st.u-gakugei.ac.jp>
2020-05-21 00:22:20 +0900

    add get config method

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,8 @@
 extern crate clap;
 extern crate git_tag_push;
-use clap::{App, Arg};
+use clap::{App, Arg, ArgMatches};
 use git_tag_push::config::Config;
 fn main() {
-    let mut config = Config::new();
     let app = App::new("git-tag-push")
         .version("0.1.0")
         .about("git tag push is cli to upgrade version keeping form of semantic versioning and push version to remote.")
@@ -34,4 +33,39 @@ fn main() {
         .subcommand(App::new("major"))
         .subcommand(App::new("minor"))
         .subcommand(App::new("patch"));
+
+    let config = get_config(&app.get_matches());
+}
+
+fn get_config(matches: &ArgMatches) -> Config {
+    let mut config = Config::new();
+
+    if let Some(p) = matches.value_of("prefix") {
+        config.prefix = String::from(p);
+    }
+
+    if let Some(s) = matches.value_of("sufix") {
+        config.sufix = String::from(s);
+    }
+
+    if matches.is_present("auto-push") {
+        config.auto_push = true;
+    }
+
+    if matches.is_present("fetch") {
+        config.fetch = true;
+    }
+
+    if let Some(_) = matches.subcommand_matches("major") {
+        config.major = true;
+    }
+
+    if let Some(_) = matches.subcommand_matches("minor") {
+        config.minor = true;
+    }
+
+    if let Some(_) = matches.subcommand_matches("patch") {
+        config.patch = true;
+    }
+    config
 }
