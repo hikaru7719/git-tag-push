847221462fbcdc23d19e43271b1c312562c0aaf9
miyahika <j148016s@st.u-gakugei.ac.jp>
2020-06-24 00:00:57 +0900

    結合テスト用のディレクトリの作成

---
+++ tests/integration_test.rs
@@ -0,0 +1,22 @@
+extern crate git_tag_push;
+use git_tag_push::git;
+use std::env;
+use std::fs;
+use std::path::Path;
+
+pub fn create_local_repository() {
+    let path = Path::new("./repository");
+    fs::create_dir(&path).unwrap();
+    env::set_current_dir(&path).unwrap();
+}
+
+pub fn delete_local_repository() {
+    let path = Path::new("./repository");
+    fs::remove_dir(&path).unwrap();
+}
+
+#[test]
+pub fn test_run() {
+    create_local_repository();
+    delete_local_repository();
+}
